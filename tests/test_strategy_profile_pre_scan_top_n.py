b21f5ee4e2f6e181c650f0a1280993a40e168762
Cesare de Padua <cesaredepadua@gmail.com>
2026-07-03 01:47:29 +0200

    Rename strategy profile pre scan top n test

--- tests/test_strategy_profile_pre_scan_top_n.py
+++ tests/test_strategy_profile_pre_scan_top_n.py
@@ -2,14 +2,14 @@ from app.instruments.models import AssetClass
 from app.strategies.balanced_strategy import BalancedStrategyConfig
 
 
-def test_strategy_profile_resolves_global_pre_scan_top_n_for_every_asset_class():
-    profile = BalancedStrategyConfig(pre_scan_top_n=3)
+def test_strategy_profile_resolves_global_candidate_selection_top_n_for_every_asset_class():
+    profile = BalancedStrategyConfig(candidate_selection_top_n=3)
 
-    crypto_config = profile.pre_scan_config_for_asset_class(AssetClass.CRYPTO)
-    equity_us_config = profile.pre_scan_config_for_asset_class(AssetClass.EQUITY_US)
-    equity_eu_config = profile.pre_scan_config_for_asset_class(AssetClass.EQUITY_EU)
+    crypto_config = profile.candidate_selection_config_for_asset_class(AssetClass.CRYPTO)
+    equity_us_config = profile.candidate_selection_config_for_asset_class(AssetClass.EQUITY_US)
+    equity_eu_config = profile.candidate_selection_config_for_asset_class(AssetClass.EQUITY_EU)
 
-    assert profile.pre_scan_top_n == 3
+    assert profile.candidate_selection_top_n == 3
     assert crypto_config.top_n == 3
     assert equity_us_config.top_n == 3
     assert equity_eu_config.top_n == 3
