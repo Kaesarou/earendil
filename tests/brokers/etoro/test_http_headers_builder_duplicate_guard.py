b9b2f88f19f47d9993435c515a4ce4e486bc740f
Cesare de Padua <cesaredepadua@gmail.com>
2026-07-05 01:53:31 +0200

    Remove temporary duplicate headers test

--- tests/brokers/etoro/test_http_headers_builder_duplicate_guard.py
+++
@@ -1,9 +0,0 @@
-from app.brokers.etoro.http_headers_builder import build_headers
-
-
-def test_temporary_duplicate_guard():
-    assert build_headers(
-        request_id='request-1',
-        api_key='public-token',
-        user_key='user-token',
-    )['Accept'] == 'application/json'
