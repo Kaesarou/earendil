3ad76113edeb2cd337aab97aa04f49c584e84cd9
Cesare de Padua <cesaredepadua@gmail.com>
2026-07-05 03:15:30 +0200

    Remove old eToro request settings helper test location

--- tests/brokers/etoro/test_request_settings.py
+++
@@ -1,9 +0,0 @@
-from app.brokers.etoro.request_settings import (
-    DEFAULT_REQUEST_TIMEOUT_SECONDS,
-    default_request_timeout_seconds,
-)
-
-
-def test_default_request_timeout_seconds():
-    assert DEFAULT_REQUEST_TIMEOUT_SECONDS == 10
-    assert default_request_timeout_seconds() == 10
