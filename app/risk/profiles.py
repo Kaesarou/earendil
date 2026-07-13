d230d07172496c47249f7d07179722ecffab0998
Cesare de Padua <cesaredepadua@gmail.com>
2026-07-14 01:10:46 +0200

    Remove unused global risk profile module

--- app/risk/profiles.py
+++
@@ -1,7 +0,0 @@
-from app.instruments.models import AssetClass, RiskProfile
-from app.strategies.balanced_strategy_config import BalancedStrategyConfig
-
-
-DEFAULT_RISK_PROFILES: dict[AssetClass, RiskProfile] = (
-    BalancedStrategyConfig().risk_profiles()
-)
